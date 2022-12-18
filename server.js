f97b6b0c8b0fd1388cf7a426f8a47de424e7a71e
Vedprakash Sinha <vedprakash4gs@gmail.com>
2022-12-18 19:13:32 +0530

    added new file

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const PORT = process.env.PORT || 3002;
-const path = require("path");
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use("api", router);
-server.listen(PORT, () =>
-  console.log(`JSON Server is running on port ${PORT}`)
-);
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('/api', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
